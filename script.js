781250030c63cab56ca84de944b8efa39947d178
laur507p <laur507p@stud.kea.dk>
2020-03-17 16:22:47 +0000

    Splitscreen snapscroll virker

---
+++ script.js
@@ -0,0 +1,49 @@
+//split scroll funktion start
+let isLeftSideScrolling;
+let container_LHasScrollSnap = false;
+const container_L = document.querySelector(".container_left");
+const container_R = document.querySelector(".container_right");
+
+// Sæt højre scroll i bund
+container_R.scrollTop = container_R.scrollHeight - container_R.clientHeight - container_R.scrollTop;
+
+
+
+container_L.addEventListener("scroll", listenL);
+// kontrollere venstre del af skærmen
+function listenL() {
+    if (isLeftSideScrolling) {
+        container_R.scrollTop = this.scrollHeight - this.clientHeight - this.scrollTop;
+
+
+        // hvis venstre side bliver scrollet tilføjes scroll snap på venstre side
+        if (container_LHasScrollSnap == true) {
+            container_L.classList.add("scroll_snap_type_y_mandatory");
+            container_R.classList.remove("scroll_snap_type_y_mandatory");
+            container_LHasScrollSnap = false;
+        }
+    }
+    isLeftSideScrolling = true;
+}
+
+container_R.addEventListener("scroll", listenR);
+// kontrollere højre del af skærmen
+function listenR() {
+
+
+    if (!isLeftSideScrolling) {
+        container_L.scrollTop = this.scrollHeight - this.clientHeight - this.scrollTop;
+
+        // hvis venstre side ikke bliver scrollet tilføjes scroll snap på højre side
+        if (container_LHasScrollSnap == false) {
+
+            container_R.classList.add("scroll_snap_type_y_mandatory");
+            container_L.classList.remove("scroll_snap_type_y_mandatory");
+            container_LHasScrollSnap = true;
+        }
+
+    }
+    isLeftSideScrolling = false;
+}
+
+//split scroll funktion slut
