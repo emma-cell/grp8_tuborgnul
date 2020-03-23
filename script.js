ab18eba897f85969af0edc710ead645d17dc954b
laur507p <laur507p@stud.kea.dk>
2020-03-23 00:44:55 +0000

    det gaar godt

--- script.js
+++ script.js
@@ -47,3 +47,66 @@ function listenR() {
 }
 
 //split scroll funktion slut
+
+// funktion til at skifte tekst
+let text = document.querySelector("#nultext");
+
+setTimeout(textSkift1, 2000);
+
+function textSkift1() {
+    console.log("textskift1")
+    text.textContent = "Konsekvenser";
+    setTimeout(textSkift2, 2000);
+}
+
+function textSkift2() {
+    text.textContent = "Fortrydelser";
+    setTimeout(textSkift3, 2000);
+}
+
+function textSkift3() {
+    text.textContent = "Drunk texts";
+    setTimeout(textSkift4, 2000);
+}
+
+function textSkift4() {
+    text.textContent = "Hovedpine";
+    setTimeout(textSkift5, 2000);
+}
+
+function textSkift5() {
+    text.textContent = "Tømmermænd";
+    setTimeout(textSkift1, 2000);
+}
+
+// MOBILE
+
+let text2 = document.querySelector("#nultextmobile");
+
+setTimeout(text2Skift1, 2000);
+
+function text2Skift1() {
+    console.log("textskift1")
+    text2.textContent = "Konsekvenser";
+    setTimeout(text2Skift2, 2000);
+}
+
+function text2Skift2() {
+    text2.textContent = "Fortrydelser";
+    setTimeout(text2Skift3, 2000);
+}
+
+function text2Skift3() {
+    text2.textContent = "Drunk texts";
+    setTimeout(text2Skift4, 2000);
+}
+
+function text2Skift4() {
+    text2.textContent = "Hovedpine";
+    setTimeout(text2Skift5, 2000);
+}
+
+function text2Skift5() {
+    text2.textContent = "Tømmermænd";
+    setTimeout(text2Skift1, 2000);
+}
