594e98059c27f365f7a6ef012dde46228bf9a030
laur507p <laur507p@stud.kea.dk>
2020-03-24 19:13:14 +0000

    næsten done

--- script.js
+++ script.js
@@ -276,5 +276,175 @@ function knapKlik5() {
 
 function spilSlut() {
     console.log("Tilykke du fik", point, "point!");
-    result.textContent = "Tilykke du fik " + point + " point!";
+    result.textContent = "Du fik " + point + " point!";
+}
+
+//------------------ QUIZ MOBILE ----------------
+
+
+let sporgsmalMobile = document.querySelector("#sporgsmalmobile");
+let knapMobile = document.querySelector("#knapmobile");
+let pointMobile = 0;
+let resultMobile = document.querySelector(".quizsvarmobile");
+
+knapMobile.addEventListener("click", knapKlikM);
+
+
+function knapKlikM() {
+    sporgsmalMobile.textContent = "2. Hvad hedder Obama til fornavn?";
+    knapMobile.removeEventListener("click", knapKlikM);
+    let svarM = document.querySelector("#inputfeltmobile").value;
+
+
+    if (svarM == "0" || svarM == "0,0" || svarM == "0%" || svarM == "0.0") {
+        console.log("Du gættede rigtigt!");
+        resultMobile.textContent = "Rigtigt!";
+        pointMobile++;
+    } else {
+        console.log("Du gættede forkert!")
+        resultMobile.textContent = "Forkert!";
+    }
+
+    knapMobile.addEventListener("click", knapKlik2M);
+
+}
+
+function knapKlik2M() {
+    sporgsmalMobile.textContent = "3. Er det okay at drikke en alkoholfri øl?";
+    knapMobile.removeEventListener("click", knapKlik2M);
+    let svarM = document.querySelector("#inputfeltmobile").value;
+
+
+    if (svarM == "Barack" || svarM == "barack") {
+        console.log("Du gættede rigtigt!");
+        resultMobile.textContent = "Rigtigt!";
+        pointMobile++;
+    } else {
+        console.log("Du gættede forkert!")
+        resultMobile.textContent = "Forkert!";
+    }
+
+    knapMobile.addEventListener("click", knapKlik3M);
+
+}
+
+function knapKlik3M() {
+    sporgsmalMobile.textContent = "4. Smager en Tuborg Nul præcis som andre øl?";
+    knapMobile.removeEventListener("click", knapKlik3M);
+    let svarM = document.querySelector("#inputfeltmobile").value;
+
+
+    if (svarM == "Ja" || svarM == "ja") {
+        console.log("Du gættede rigtigt!");
+        resultMobile.textContent = "Rigtigt!";
+        pointMobile++;
+    } else {
+        console.log("Du gættede forkert!")
+        resultMobile.textContent = "Forkert!";
+    }
+
+    knapMobile.addEventListener("click", knapKlik4M);
+
+}
+
+function knapKlik4M() {
+    sporgsmalMobile.textContent = "5. Hvad hedder Hitler til fornavn?";
+    knapMobile.removeEventListener("click", knapKlik4M);
+    let svarM = document.querySelector("#inputfeltmobile").value;
+
+
+    if (svarM == "Ja" || svarM == "ja") {
+        console.log("Du gættede rigtigt!");
+        resultMobile.textContent = "Rigtigt!";
+        pointMobile++;
+    } else {
+        console.log("Du gættede forkert!")
+        resultMobile.textContent = "Forkert!";
+    }
+
+    knapMobile.addEventListener("click", knapKlik5M);
+
+}
+
+function knapKlik5M() {
+    knapMobile.removeEventListener("click", knapKlik5M);
+    let svarM = document.querySelector("#inputfeltmobile").value;
+
+    if (svarM == "Adolf" || svarM == "adolf") {
+        console.log("Du gættede rigtigt!");
+        pointMobile++;
+        resultMobile.textContent = "Rigtigt!";
+    } else {
+        console.log("Du gættede forkert!")
+        resultMobile.textContent = "Forkert!";
+    }
+
+    spilSlutM();
+}
+
+function spilSlutM() {
+    console.log("Tilykke du fik", pointMobile, "point!");
+    resultMobile.textContent = "Du fik " + pointMobile + " point!";
+}
+
+
+
+//// KONKURRENCE
+
+
+
+
+
+let kodeKnap = document.querySelector(".kodebutton")
+
+let kodeKnap2 = document.querySelector(".kodebuttonmobile")
+
+let kodeMessage = document.querySelector(".kodemessage")
+
+let kodeMessage2 = document.querySelector(".kodemessagemobile")
+
+kodeKnap.addEventListener("click", checkKode);
+kodeKnap2.addEventListener("click", checkKode2);
+
+function checkKode() {
+    console.log("checkKode")
+
+    let kodeSvar = document.querySelector(".inputkode").value;
+
+    if (kodeSvar == "laura" || kodeSvar == "markus" || kodeSvar == "carl" || kodeSvar == "emma" || kodeSvar == "bertram") {
+        kodeMessage.textContent = "Tillykke!!!!! Indtast din email herunder til at få din præmie!"
+        document.querySelector(".inputemail").classList.remove("hide");
+    } else {
+        kodeMessage.textContent = "Ingen præmie på denne kode din taber :("
+        document.querySelector(".inputemail").classList.add("hide");
+    }
+
+}
+
+function checkKode2() {
+    console.log("checkKode2")
+
+    let kodeSvar2 = document.querySelector(".inputkodemobile").value;
+
+    if (kodeSvar2 == "laura" || kodeSvar2 == "markus" || kodeSvar2 == "carl" || kodeSvar2 == "emma" || kodeSvar2 == "bertram") {
+        kodeMessage2.textContent = "Tillykke!!!!! Indtast din email herunder til at få din præmie!"
+        document.querySelector(".inputemailmobile").classList.remove("hide");
+    } else {
+        kodeMessage2.textContent = "Ingen præmie på denne kode din taber :("
+        document.querySelector(".inputemailmobile").classList.add("hide");
+    }
+
+}
+
+
+// burgermenu
+
+const btn = document.querySelector("#toggle");
+const overlay = document.querySelector("#overlay")
+
+btn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    btn.classList.toggle("active");
+    overlay.classList.toggle("open")
 }
